6cd7e035b68f13f29f0973b98f645074ac37c302
jinnic <yoohyojin@gmail.com>
2020-05-31 19:15:28 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+
+    def call(env)
+        now = Time.now
+        resp = Rack::Response.new
+
+        if now.hour >=12
+            resp.write "Good Afternoon"
+        else
+            resp.write "Good Morning"
+        end
+        
+        resp.finish
+
+    end
+end
